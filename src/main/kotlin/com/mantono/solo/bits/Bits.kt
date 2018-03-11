8814edc12b46e30f596e2580c6f04d400d753837
Anton Österberg <anton.osterberg@gmail.com>
2018-03-11 15:41:48 +0100

    More on bits.

--- src/main/kotlin/com/mantono/solo/bits/Bits.kt
+++ src/main/kotlin/com/mantono/solo/bits/Bits.kt
@@ -1,15 +1,13 @@
 package com.mantono.solo.bits
 
+import java.nio.ByteBuffer
 import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.experimental.and
 import kotlin.experimental.or
 
-data class Bits(private val bits: BooleanArray = BooleanArray(0)): Iterable<Boolean> by bits.asIterable()
+data class Bits(private val bits: BooleanArray = BooleanArray(0)): Iterable<Boolean> by bits.asIterable(), Number()
 {
 	constructor(bits: Int): this(BooleanArray(bits) { false })
-
-	constructor(bits: List<Number>): this(bits.map { it.toByte() }.map { it > 0 }.toBooleanArray())
 
 	val size: Int = bits.size
 
@@ -39,7 +37,84 @@ data class Bits(private val bits: BooleanArray = BooleanArray(0)): Iterable<Bool
 		return Bits(newArray)
 	}
 
+	fun reduceToMostSignificantSetBit(): Bits
+	{
+		val i: Int = bits.indexOf(true)
+		if(i < 0)
+			return Bits(0)
+
+		return Bits(bits.sliceArray(i .. bits.lastIndex))
+	}
+
+	fun padLeftWithZeroes(leftPads: Int): Bits
+	{
+		if(leftPads < 0)
+			throw IllegalArgumentException("Negative padding not allowed")
+		if(leftPads == 0)
+			return this
+		val paddedArray = BooleanArray(leftPads + this.size) { i: Int ->
+			val index = i - leftPads
+			if(index < 0)
+				false
+			else
+				this[index]
+		}
+
+		return Bits(paddedArray)
+	}
+
 	fun toByteArray(): ByteArray = bitsToBytes(bits).toByteArray()
+
+	override fun toByte(): Byte = asNumber()
+	override fun toChar(): kotlin.Char = toByte().toChar()
+	override fun toDouble(): Double = Double.fromBits(toLong())
+	override fun toFloat(): Float = Float.fromBits(toInt())
+	override fun toInt(): Int = asNumber()
+	override fun toShort(): Short = asNumber()
+
+	override fun toLong(): Long
+	{
+		val buffer: ByteBuffer = ByteBuffer.allocate(8)
+		buffer.put(toByteArray())
+		buffer.flip()
+		return buffer.long
+	}
+
+	inline fun <reified T: kotlin.Number> asNumber(): T
+	{
+		val readSize: Int = when(T::class)
+		{
+			Long::class, Double::class -> 8
+			Int::class, Float::class -> 4
+			Short::class -> 2
+			Char::class, Byte::class -> 1
+			else -> 0
+		}
+
+		val buffer = ByteBuffer.allocate(8)
+		buffer.put(toByteArray())
+		buffer.flip()
+
+		val readFromIndex: Int = 8 - readSize
+		return when(T::class)
+		{
+			Long::class -> buffer.getLong(readFromIndex)
+			Double::class -> buffer.getDouble(readFromIndex)
+			Int::class -> buffer.getInt(readFromIndex)
+			Float::class -> buffer.getFloat(readFromIndex)
+			Short::class -> buffer.getShort(readFromIndex)
+			Char::class -> buffer.getChar(readFromIndex)
+			Byte::class -> buffer.get(readFromIndex)
+			else -> IllegalArgumentException("Unexpected type: ${T::class.qualifiedName}")
+		} as T
+	}
+
+	fun limitToSize(size: Int): Bits
+	{
+		if(size >= this.size) return this
+		val startIndex = this.size - size
+		return bits.sliceArray(startIndex..bits.lastIndex).let { Bits(it) }
+	}
 }
 
 internal tailrec fun bitsToBytes(bits: BooleanArray, bytes: Deque<Byte> = LinkedList(), byteSequence: Int = 0): Deque<Byte>
@@ -56,14 +131,63 @@ internal tailrec fun bitsToBytes(bits: BooleanArray, bytes: Deque<Byte> = Linked
 			.mapIndexed { index: Int, b: Boolean ->
 				index to (if(b) 1 else 0)
 			}
-			.onEach { println(it) }
 			.map { (index: Int, value: Int) ->
 				(value shl (rangeSize - index)).toByte()
 			}
-			.onEach { println(it) }
 			.reduce { acc, byte -> acc or byte }
 
 	bytes.addLast(singleByte)
 
 	return bitsToBytes(bits, bytes, byteSequence + 1)
 }
+
+val bitMasks: List<Byte> = listOf(
+		0b1000_0000,
+		0b0100_0000,
+		0b0010_0000,
+		0b0001_0000,
+		0b0000_1000,
+		0b0000_0100,
+		0b0000_0010,
+		0b0000_0001
+)
+		.map { it.toByte() }
+
+fun bitsOf(vararg booleans: Boolean): Bits = Bits(booleans.toList().toBooleanArray())
+
+fun bitsOf(bits: List<Number>): Bits = bits
+		.map { it.toByte() }
+		.map { it > 0 }.toBooleanArray()
+		.let { Bits(it) }
+
+private val binaryChars: List<Char> = listOf('0', '1')
+
+fun bitsOf(bits: String): Bits = bits.asSequence()
+		.filter { it in binaryChars }
+		.map { it == '1' }
+		.toList()
+		.toBooleanArray()
+		.let { Bits(it) }
+
+fun bitsOf(n: Int): Bits = bitsOf(n.toLong())
+
+fun bitsOf(n: Long): Bits = n.toByteArray().let { bitsOf(it) }
+
+fun Long.toByteArray(): ByteArray
+{
+	val buffer: ByteBuffer = ByteBuffer.allocate(8)
+	buffer.putLong(this)
+	return buffer.array()
+}
+
+fun bitsOf(bytes: ByteArray): Bits = bytes
+		.map { byte ->
+			bitMasks
+					.map { mask: Byte ->
+						byte and mask
+					}
+					.map { it != 0.toByte() }
+		}
+		.flatten()
+		.toBooleanArray()
+		.let { Bits(it) }
--- src/test/kotlin/com/mantono/solo/BitsTest.kt
+++ src/test/kotlin/com/mantono/solo/BitsTest.kt
@@ -1,17 +1,23 @@
 package com.mantono.solo
 
 import com.mantono.solo.bits.Bits
+import com.mantono.solo.bits.bitsOf
+import com.mantono.solo.bits.toByteArray
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class BitsTest
 {
+	private val booleans11 = booleanArrayOf(true, false, true, true)
+
+	// 0111 1011 --> 64 + 32 + 16 + 8 + 2 + 1 = 123
+	private val booleans123 = booleanArrayOf(false, true, true, true, true, false, true, true)
+
 	@Test
 	fun testBitsToByteArrayOnHalfByte()
 	{
-		// 0000 1011 --> 8 + 2 + 1 = 11
-		val bools = booleanArrayOf(true, false, true, true)
-		val bits: Bits = Bits(bools)
+		val bits: Bits = Bits(booleans11)
 
 		val byte: ByteArray = bits.toByteArray()
 		assertEquals(1, byte.size)
@@ -21,10 +27,7 @@ class BitsTest
 	@Test
 	fun testBitsToByteArrayOnFullByte()
 	{
-		// 0111 1011 --> 64 + 32 + 16 + 8 + 2 + 1 = 123
-		val bools = booleanArrayOf(false, true, true, true, true, false, true, true)
-		val bits: Bits = Bits(bools)
-
+		val bits: Bits = Bits(booleans123)
 		val byte: ByteArray = bits.toByteArray()
 		assertEquals(1, byte.size)
 		assertEquals(123, byte.first())
@@ -34,15 +37,106 @@ class BitsTest
 	@Test
 	fun testBitsToByteArrayOnTwoBytes()
 	{
-		// 0111 1011 --> 64 + 32 + 16 + 8 + 2 + 1 = 123
-		val bools = listOf(
+		val bits = bitsOf(listOf(
 				0, 1, 1, 1,	1, 0, 1, 1,
 				0, 1, 0, 1,	0, 1, 1, 1
-				)
-		val bits: Bits = Bits(bools)
+				))
 
 		val byte: ByteArray = bits.toByteArray()
 		assertEquals(2, byte.size)
 		assertEquals(123, byte.first())
 	}
+
+	@Test
+	fun testLongCreatorWith1()
+	{
+		val b = bitsOf(1L)
+		assertEquals(1L, b.toLong())
+	}
+
+	@Test
+	fun testLongCreatorWith15()
+	{
+		val b = bitsOf(16L)
+		assertEquals(16L, b.toLong())
+	}
+	@Test
+	fun testLongCreatorWith128()
+	{
+		val b = bitsOf(128L)
+		assertEquals(128L, b.toLong())
+	}
+
+	@Test
+	fun testLongToByteArrayWith16()
+	{
+		val bytes: ByteArray = 16L.toByteArray()
+		val expected: ByteArray = listOf(
+				0, 0, 0, 0, 0, 0, 0, 16
+		)
+				.map { it.toByte() }
+				.toByteArray()
+
+		assertArrayEquals(expected, bytes, printArray(bytes))
+	}
+
+	@Test
+	fun testLongToByteArray()
+	{
+		val bytes: ByteArray = 15L.toByteArray()
+		val expected: ByteArray = listOf(
+				0, 0, 0, 0, 0, 0, 0, 15
+		)
+				.map { it.toByte() }
+				.toByteArray()
+
+		assertArrayEquals(expected, bytes)
+	}
+
+	@Test
+	fun testIntToByteArrayToInt()
+	{
+		val i32: Int = bitsOf(32).toInt()
+		assertEquals(32, i32)
+	}
+
+	@Test
+	fun testIntToByteArrayToShort()
+	{
+		val s32: Short = bitsOf(32).toShort()
+		assertEquals(32.toShort(), s32)
+	}
+
+	@Test
+	fun testIntToByteArrayToShortOnSeveralBytes()
+	{
+		val s256: Short = bitsOf(256).toShort()
+		assertEquals(256.toShort(), s256)
+	}
+
+	@Test
+	fun testLongToByteArrayToByteWithinBounds()
+	{
+		val b32: Byte = bitsOf(32L).toByte()
+		assertEquals(32.toByte(), b32)
+	}
+
+	@Test
+	fun testLongToByteArrayToByteWithOverflow()
+	{
+		// 1011 0000 0110 = 2048 + 512 + 256 + 4 + 2 = 2822
+		// 1011 is outside byte range, so expected is 0000 0110 which equals to 6
+		val b6: Byte = bitsOf(2822).toByte()
+		assertEquals(6.toByte(), b6)
+	}
+
+	private fun printArray(arr: ByteArray) = arr
+			.mapIndexed { index, byte -> index to byte }
+			.joinToString(separator = " ") { (index: Int, byte: Byte) ->
+				when{
+					index % 7 == 0 -> "$byte\n"
+					index % 4 == 0 -> "_${byte}"
+					else -> byte.toString()
+				}
+			}
 }
